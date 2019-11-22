cced08c81d33edcc29742876aebcd21a67569b4a
Zeuxis Lo <seekstudio@gmail.com>
2019-11-22 16:50:05 +0800

    upgrade gulp to 4 and jQuery to 3

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,17 @@ var gulp = require('gulp'),
     rename = require('gulp-rename')
     sequence = require('gulp-sequence');
 
-gulp.task('concat:css', function() {
+// Actions
+function concatCSS() {
     return gulp.src([
-                'node_modules/bootstrap/dist/css/bootstrap.css',
-                'assets/css/app.css',
-           ])
-           .pipe(concat('bundle.css'))
-           .pipe(gulp.dest('source/css/'));
-});
+            'node_modules/bootstrap/dist/css/bootstrap.css',
+            'assets/css/app.css',
+       ])
+       .pipe(concat('bundle.css'))
+       .pipe(gulp.dest('source/css/'));
+}
 
-gulp.task('concat:js', function() {
+function concatJS() {
     return gulp.src([
                 'node_modules/jquery/dist/jquery.js',
                 'node_modules/bootstrap/dist/js/bootstrap.js',
@@ -22,33 +23,44 @@ gulp.task('concat:js', function() {
            ])
            .pipe(concat('bundle.js'))
            .pipe(gulp.dest('source/js/'));
-});
+}
 
-gulp.task('copy:bootstrap:fonts', function() {
+function copyBootstrapFonts() {
     return gulp.src('node_modules/bootstrap/dist/fonts/*')
-               .pipe(gulp.dest('source/fonts/'))
-});
+               .pipe(gulp.dest('source/fonts/'));
+}
 
-gulp.task('compress:css', function() {
+function compressCSS() {
     return gulp.src('source/css/bundle.css')
                .pipe(cssmin())
                .pipe(rename('bundle.min.css'))
-               .pipe(gulp.dest('source/css/'))
-});
+               .pipe(gulp.dest('source/css/'));
+}
 
-gulp.task('compress:js', function() {
+function compressJS() {
     return gulp.src('source/js/bundle.js')
                .pipe(uglify())
                .pipe(rename('bundle.min.js'))
-               .pipe(gulp.dest('source/js/'))
+               .pipe(gulp.dest('source/js/'));
+}
+
+// Watcher
+function watch() {
+    gulp.watch('assets/css/*.css', bundleCSS);
+    gulp.watch('assets/js/*.js', bundleJS);
+}
+
+// Bundle Actions
+const bundleCSS = gulp.series(concatCSS, copyBootstrapFonts, compressCSS, function finishBundleCSS(done) {
+    done();
 });
 
-gulp.task('bundle:css', sequence(['concat:css', 'copy:bootstrap:fonts'], 'compress:css'));
-gulp.task('bundle:js', sequence('concat:js', 'compress:js'));
-
-gulp.task('default', sequence('bundle:css', 'bundle:js'));
-
-gulp.task('watch', function() {
-    gulp.watch('assets/css/*.css', ['bundle:css']);
-    gulp.watch('assets/js/*.js', ['bundle:js']);
+const bundleJS = gulp.series(concatJS, compressJS, function finishBundleJS(done) {
+    done();
 });
+
+// Default action
+var build = gulp.parallel(bundleCSS, bundleJS);
+
+// Exports
+exports.default = build;
